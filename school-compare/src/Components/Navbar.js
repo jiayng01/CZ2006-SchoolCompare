43e4de6fe9cfd6d0f4715f892de90c66d214397b
Li Han <wong.lihan48@gmail.com>
2022-03-29 23:32:40 +0800

    css changes

--- school-compare/src/Components/Navbar.js
+++ school-compare/src/Components/Navbar.js
@@ -114,11 +114,22 @@ function Navbar() {
                 >
                   Log In
                 </NavLink>
+                <li className="navbar-list">
+                  <img
+                    src={avatar}
+                    alt="avatar"
+                    className="navbar-list navbar-avatar"
+                  />
+                </li>
               </li>
 
-              <li className="navbar-list">
-                <img src={avatar} alt="avatar" className="navbar-avatar" />
-              </li>
+              {/* <li className="navbar-list">
+                <img
+                  src={avatar}
+                  alt="avatar"
+                  className="navbar-list navbar-avatar"
+                />
+              </li> */}
             </>
           )}
 
