df175ef5c2ac92fc3b7f687a55b09c345dbffa69
Li Han <wong.lihan48@gmail.com>
2022-04-06 00:58:32 +0800

    fixed some css + moe programme for more info

--- school-compare/src/Components/MoreInformation.js
+++ school-compare/src/Components/MoreInformation.js
@@ -1,5 +1,3 @@
-
-
 // import "../ComponentsCSS/MoreInformation.css";
 // import React from "react";
 
@@ -9,13 +7,12 @@
 //       className="more-info-button"
 //       onClick={() => console.log("button clicked")}
 //     >
-//       Click here for more information 
+//       Click here for more information
 //     </button>
 //   );
 // }
 
 // export default MoreInfoButton;
-
 
 //import data from "../JSON/combined_data.json";
 import react from "react";
@@ -30,14 +27,13 @@ import { useLocation, useParams } from "react-router-dom";
 import { SchoolsContext } from "../Contexts/SchoolsContext";
 import { useContext } from "react";
 
- 
-
 const MoreInformation = () => {
-   const { schoolsContext } = useContext(SchoolsContext);
-   let data = schoolsContext.schools;
+  const { schoolsContext } = useContext(SchoolsContext);
+  let data = schoolsContext.schools;
   //console.log("school id", props.location.state);
   const { school_name } = useParams();
   //console.log({id})
+
   return (
     <>
       <div className="more-info-heading"> {school_name} </div>
@@ -45,6 +41,14 @@ const MoreInformation = () => {
         {data
           .filter((value) => value.school_name === `${school_name}`)
           .map((props) => {
+            let subjects = [];
+            for (let i = 0; i < props.subject_desc.length; i++) {
+              if (i !== props.subject_desc.length - 1) {
+                subjects.push(props.subject_desc[i].toLowerCase() + " ,  ");
+              } else {
+                subjects.push(props.subject_desc[i].toLowerCase());
+              }
+            }
             return (
               <div key={props._id}>
                 <MoreInfo
@@ -59,12 +63,22 @@ const MoreInformation = () => {
                   bus_desc={props.bus_desc}
                   principal_name={props.principal_name}
                   url_address={props.url_address}
-                  subjects={props.subject_desc}
-                  physical_sports={props.physical_sports} visual_and_pa={props.visual_and_pa} clubs_and_societies={props.clubs_and_societies} uniformed_groups={props.uniformed_groups} others={props.others}
-                  />
+                  subjects={subjects}
+                  physical_sports={props.physical_sports}
+                  visual_and_pa={props.visual_and_pa}
+                  clubs_and_societies={props.clubs_and_societies}
+                  uniformed_groups={props.uniformed_groups}
+                  others={props.others}
+                  moe_programme={props.moe_programme}
+                  alp_domain={props.alp_domain}
+                  alp_title={props.alp_title}
+                  llp_domain1={props.llp_domain1}
+                  llp_title1={props.llp_title1}
+                  llp_domain2={props.llp_domain2}
+                  llp_title2={props.llp_title2}
+                />
               </div>
             );
-             
           })}
       </div>
     </>
@@ -82,8 +96,18 @@ const MoreInfo = ({
   principal_name,
   url_address,
   subjects,
-  physical_sports,visual_and_pa,clubs_and_societies,uniformed_groups,others,
-  moe_programme, alp_domain, alp_title, llp_domain1, llp_title1, llp_domain2, llp_title2
+  physical_sports,
+  visual_and_pa,
+  clubs_and_societies,
+  uniformed_groups,
+  others,
+  moe_programme,
+  alp_domain,
+  alp_title,
+  llp_domain1,
+  llp_title1,
+  llp_domain2,
+  llp_title2,
 }) => {
   if (!_id) return <div />;
   return (
@@ -91,24 +115,24 @@ const MoreInfo = ({
       <div className="school-info">
         <p className="gen-info ">General Information</p>
 
-        <div className="school-location">
+        <div className="school-location-mf">
           <FontAwesomeIcon
-            className="fa-location-dot-icon"
+            className="fa-location-dot-icon-mf"
             icon={faLocationDot}
           />
 
-          <span className="school-address">{address + " ,"}</span>
-          <div className="school-postal">{"S" + postal_code}</div>
+          <span className="school-address-mf">{address + " ,"}</span>
+          <div className="school-postal-mf">{"S" + postal_code}</div>
         </div>
 
-        <div className="school-mrt-wrapper">
+        <div className="school-mrt-wrapper-mf">
           <FontAwesomeIcon
             className="fa-train-subway-icon"
             icon={faTrainSubway}
           />
           <img className="mrt-icon" src={mrtIcon} alt="mrt icon" />
 
-          <div className="school-mrt-desc">{mrt_desc}</div>
+          <div className="school-mrt-desc-mf">{mrt_desc}</div>
         </div>
 
         <div className="school-bus-wrapper">
@@ -139,81 +163,85 @@ const MoreInfo = ({
             <a href={url_address}>Visit the School Page </a>
           </b>
         </div>
-      </div> {/* div for general info */}
-
-      <div className="subjects-card">
-      <p className="subjects-card-school-name"> Subjects Offered
-      <div className="subjects-desc">{subjects}</div></p>
-    </div>{/* div for subjects*/ }
-
-      <div className="cca-card">
+      </div>{" "}
+      {/* div for general info */}
+      <div className="subjects-card-mf">
+        <p className="subjects-card-school-name-mf">
+          {" "}
+          Subjects Offered
+          <div className="subjects-desc-mf">{subjects}</div>
+        </p>
+      </div>
+      {/* div for subjects*/}
+      <div className="cca-card-mf">
         {
-          <div className="cca-container">
-            CCA Information 
-            {
-              <div className="cca-div">
-                <div className="cca-category">Physical Sports </div>
-                <div className="cca-name"> {physical_sports + ","}</div>
+          <div className="cca-container-mf">
+            CCA Information
+            {physical_sports !== undefined && (
+              <div className="cca-div-mf">
+                <div className="cca-category-mf">Physical Sports </div>
+                <div className="cca-name-mf"> {physical_sports + ","}</div>
               </div>
-            }
-            {
-              <div className="cca-div">
-                <div className="cca-category">Visual & Performing Arts </div>
-                <div className="cca-name"> {visual_and_pa + ","}</div>
+            )}
+            {visual_and_pa !== undefined && (
+              <div className="cca-div-mf">
+                <div className="cca-category-mf">Visual & Performing Arts </div>
+                <div className="cca-name-mf"> {visual_and_pa + ","}</div>
               </div>
-            }
-            {
-              <div className="cca-div">
-                <div className="cca-category">Clubs & Societies </div>
-                <div className="cca-name"> {clubs_and_societies + ","} </div>
+            )}
+            {clubs_and_societies !== undefined && (
+              <div className="cca-div-mf">
+                <div className="cca-category-mf">Clubs & Societies </div>
+                <div className="cca-name-mf"> {clubs_and_societies + ","} </div>
               </div>
-            }
-            {
-              <div className="cca-div">
-                <div className="cca-category">Uniformed Groups </div>
-                <div className="cca-name">{uniformed_groups + ","} </div>
+            )}
+            {uniformed_groups !== undefined && (
+              <div className="cca-div-mf">
+                <div className="cca-category-mf">Uniformed Groups </div>
+                <div className="cca-name-mf">{uniformed_groups + ","} </div>
               </div>
-            }
-            {
-              <div className="cca-div">
-                <div className="cca-category">Others </div>
-                <div className="cca-name"> {others + ","} </div>
+            )}
+            {others !== undefined && (
+              <div className="cca-div-mf">
+                <div className="cca-category-mf">Others </div>
+                <div className="cca-name-mf"> {others + ","} </div>
               </div>
-            }
-          </div>
-        }
-      </div> {/* div for cca*/}
-
-      <div className="elective-card">
-      <div className="elective-container">
-      MOE Programme 
-        {
-          <div className="elective-div">
-            <div className="elective-category"> </div>
-            <div className="elective-name"> {moe_programme} </div>
-          </div>
-        }
-        {
-          <div className="elective-div">
-            <div className="elective-category">{alp_domain} </div>
-            <div className="elective-name"> {alp_title}</div>
-          </div>
-        }
-        {
-          <div className="elective-div">
-            <div className="elective-category">{llp_domain1} </div>
-            <div className="elective-name"> {llp_title1} </div>
-          </div>
-        }
-        {
-          <div className="elective-div">
-            <div className="elective-category">{llp_domain2} </div>
-            <div className="elective-name">{llp_title2} </div>
+            )}
           </div>
         }
       </div>
-    </div> {/*div for electives*/}
-</div>
+      {/* div for cca*/}
+      <div className="elective-card-mf">
+        <div className="elective-container-mf">
+          MOE Programme
+          {
+            <div className="elective-div-mf">
+              <div className="elective-category-mf"> </div>
+              <div className="elective-name-mf"> {moe_programme} </div>
+            </div>
+          }
+          {alp_domain != "NULL" && (
+            <div className="elective-div-mf">
+              <div className="elective-category-mf">{alp_domain} </div>
+              <div className="elective-name-mf"> {alp_title}</div>
+            </div>
+          )}
+          {llp_domain1 != "NULL" && (
+            <div className="elective-div-mf">
+              <div className="elective-category-mf">{llp_domain1} </div>
+              <div className="elective-name-mf"> {llp_title1} </div>
+            </div>
+          )}
+          {llp_domain2 != "NULL" && (
+            <div className="elective-div-mf">
+              <div className="elective-category-mf">{llp_domain2} </div>
+              <div className="elective-name-mf">{llp_title2} </div>
+            </div>
+          )}
+        </div>
+      </div>{" "}
+      {/*div for electives*/}
+    </div>
   );
 };
 
