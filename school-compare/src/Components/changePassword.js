7b367c05a69f66a2c119818a8745f04b5082b758
Li Han <wong.lihan48@gmail.com>
2022-04-09 16:54:22 +0800

    minor changes

--- school-compare/src/Components/changePassword.js
+++ school-compare/src/Components/changePassword.js
@@ -4,46 +4,56 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import "../PagesCSS/Login.css";
 import { toast } from "react-toastify";
 import { updatePassword } from "firebase/auth";
-import BackgroundParticle from "../Components/BackgroundParticle";
+import BackgroundParticle from "./BackgroundParticle";
 
-function ChangePassword(){
+function ChangePassword() {
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [currentUser, loading] = useAuthState(auth);
 
-    const [currentPassword, setCurrentPassword] = useState("");
-    const [newPassword, setNewPassword] = useState("");
-    const [currentUser, loading] = useAuthState(auth);
-    
-    const changeOnClick = () => {
-        reauthenticate(currentPassword).then(() => {
-            updatePassword(currentUser, newPassword).then(() => {
-                toast("Password was changed.", { type: "success" });
-            }).catch((error) => { 
-                toast(error.message, { type: "error" });
-            });
-        }).catch((error) => { toast(error.message, { type: "error" }); });
-    }
+  const changeOnClick = () => {
+    reauthenticate(currentPassword)
+      .then(() => {
+        updatePassword(currentUser, newPassword)
+          .then(() => {
+            toast("Password was changed.", { type: "success" });
+          })
+          .catch((error) => {
+            toast(error.message, { type: "error" });
+          });
+      })
+      .catch((error) => {
+        toast(error.message, { type: "error" });
+      });
+  };
 
-    return (
+  return (
     <div className="login">
-        <BackgroundParticle />
-        <div className="login__container">
-            <input
-             type="password"
-             className="login__textBox"
-             value={currentPassword}
-             onChange={(e) => setCurrentPassword(e.target.value)}
-             placeholder="Current Password"
-             />
-            <input
-             type="password"
-             className="login__textBox"
-             value={newPassword}
-             onChange={(e) => setNewPassword(e.target.value)}
-             placeholder="New Password"
-             />
-             <button className="login__btn" onClick={changeOnClick} disabled={loading}>
-                 Change Password
-             </button>
-        </div>;
+      <BackgroundParticle />
+      <div className="login__container">
+        <input
+          type="password"
+          className="login__textBox"
+          value={currentPassword}
+          onChange={(e) => setCurrentPassword(e.target.value)}
+          placeholder="Current Password"
+        />
+        <input
+          type="password"
+          className="login__textBox"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
+          placeholder="New Password"
+        />
+        <button
+          className="login__btn"
+          onClick={changeOnClick}
+          disabled={loading}
+        >
+          Change Password
+        </button>
+      </div>
+      ;
     </div>
   );
 }
